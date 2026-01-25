7d9f69f0bff32b2eda8ef6fb70216c968300705f
Nanashi137 <c0mputereng1neerstudent0@gmail.com>
2026-01-25 10:30:17 +0700

    feat: added llm client

---
+++ backend/clients/llm.py
@@ -0,0 +1,24 @@
+import os
+
+from langchain_openai import ChatOpenAI
+
+
+def get_chat_model():
+    api_key = os.getenv("OPENAI_API_KEY", "")
+    model_name = os.getenv("OPENAI_MODEL_NAME", "gpt-3.5-turbo")
+    temperature = float(os.getenv("OPENAI_TEMPERATURE", "0.7"))
+
+    chat_model = ChatOpenAI(
+        model_name=model_name, temperature=temperature, api_key=api_key
+    )
+
+    return chat_model
+
+
+if __name__ == "__main__":
+    import dotenv
+
+    dotenv.load_dotenv()
+    chat_model = get_chat_model()
+
+    print(chat_model.invoke("hello, world!"))
